c9dc51a019c53da50227343572806cdf0cd9d384
anup ghunawat <anupghunawat10@gmail.com>
2024-12-10 14:20:11 +0530

    Implement UI for Rock-paper-scissors game with score display

--- script.js
+++ script.js
@@ -1,37 +1,87 @@
-
-
-//step1 --> get computer's choice
-
-const choice = Math.floor(Math.random() * 3) +1;
-
-//step2 --> get user's choice
-
-
-
-let playerScore = 0; 
+const btn = document.querySelectorAll('button');
+const result = document.querySelector('.scores');
+const pScore = document.querySelector('#playerScore');
+const cScore = document.querySelector('#ComputerScore');
+const container = document.querySelector('.container');
 let computerScore = 0;
+let playerScore = 0;
 
-for(let i = 0; i< 3; i++) {
-    const userChoice = Number(prompt("choose 1 for Rock, 2 for paper and 3 for scissor"))
 
-    if((userChoice === 2 && choice === 1) && (userChoice === 1 && choice === 3) && (userChoice === 3 && choice === 2)) {
-        console.log("You win");
-        playerScore += 1;
-    } else if(userChoice === choice) {
-        console.log("TIE!!");
-    } else {
-        console.log("YOU LOSE");
-        computerScore += 1;
-    }
+function playRound(e) {
+  
     
-    console.log(`Computer's choice was ${choice}`);
-    console.log(`your score is ${playerScore} and your opponent's score is ${computerScore}`);
+    const hand = Math.floor(Math.random() * 3)+1;
+    let choice;
+    if(hand === 1) choice = 'Rock';
+    if(hand === 2) choice = 'Paper';
+    if(hand === 3) choice = 'Scissor';
+    const userChoice = e.target.value;
+
+    if(playerScore === 5 || computerScore === 5) {
+        const winner = playerScore === 5 ? 'Player' : 'Computer';
+        container.style
+        result.textContent = `${winner} won the game!`;
+        btn.forEach((button) => button.disabled = true);
+        return;
+    }
+
+    if (
+        (userChoice === 'Rock' && choice === 'Scissor') || // Rock beats Scissors
+        (userChoice === 'Paper' && choice === 'Rock') || // Paper beats Rock
+        (userChoice === 'Scissor' && choice === 'Paper')    // Scissors beats Paper
+        ) {
+            playerScore++;
+            pScore.textContent = `Player : ${playerScore}`;
+        } else if(userChoice == choice) {
+            alert("tie");
+        } else {
+            computerScore++;
+            cScore.textContent = `Player 2 : ${computerScore}`;
+        }
+        console.log(`computer's choice was ${choice}`);
+        console.log(`yourchoice was ${userChoice}`);
+        console.log(`playerscore is ${playerScore}`);
+        console.log(`computerscore is ${computerScore}`);
+    
 }
 
-if(playerScore > computerScore) {
-    console.log("user has won");
-}else {
-    console.log("computer has won");
-}
-console.log('round is over');
+btn.forEach((button) => {
+    button.addEventListener('click', playRound);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
